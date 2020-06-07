d1b0aa1f66746a7ac488d260acdd72361d95aef2
Jeremy Sim <jsim0809@gmail.com>
2020-06-07 16:21:24 -0700

    Complete gallery styling

--- client/Gallery.jsx
+++ client/Gallery.jsx
@@ -9,47 +9,47 @@ function Gallery() {
         <div className="gallery-item-text">
           <a className="gallery-item-title">ALL EAT CANDY</a>
           <h4 className="gallery-item-author">Ezra Li Parker</h4>
-          <p className="gallery-item-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit excepturi quis vel cum quod doloribus nulla at dolor aliquid, consequatur illum praesentium deleniti optio rerum, ullam voluptas! Incidunt, vel asperiores?</p>
+          <p className="gallery-item-blurb">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit excepturi quis vel cum quod doloribus nulla at dolor aliquid, consequatur illum praesentium deleniti optio rerum, ullam voluptas! Incidunt, vel asperiores?</p>
         </div>
       </div>
       <div className="gallery-item">
-        <div className="thumb-1"></div>
+        <div className="gallery-item-thumbnail"></div>
         <div className="gallery-item-text">
           <a className="gallery-item-title">DOG EAT DOG</a>
           <h4 className="gallery-item-author">Susan F. Banks</h4>
-          <p className="gallery-item-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime distinctio alias ad corporis velit ullam ut sunt, voluptates vitae, quae, blanditiis voluptatum vel molestiae odio provident dolores quas aspernatur deserunt.</p>
+          <p className="gallery-item-blurb">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime distinctio alias ad corporis velit ullam ut sunt, voluptates vitae, quae, blanditiis voluptatum vel molestiae odio provident dolores quas aspernatur deserunt.</p>
         </div>
       </div>
       <div className="gallery-item">
-        <div className="thumb-1"></div>
+        <div className="gallery-item-thumbnail"></div>
         <div className="gallery-item-text">
           <a className="gallery-item-title">GODS EAT GODS</a>
           <h4 className="gallery-item-author">Peter Pan</h4>
-          <p className="gallery-item-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi corporis modi in! Sequi exercitationem, adipisci doloremque facere, quia necessitatibus, debitis minima voluptatum doloribus minus deleniti repudiandae ut repellat id amet!</p>
+          <p className="gallery-item-blurb">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi corporis modi in! Sequi exercitationem, adipisci doloremque facere, quia necessitatibus, debitis minima voluptatum doloribus minus deleniti repudiandae ut repellat id amet!</p>
         </div>
       </div>
       <div className="gallery-item">
-        <div className="thumb-1"></div>
+        <div className="gallery-item-thumbnail"></div>
         <div className="gallery-item-text">
           <a className="gallery-item-title">BLUE'S CLUES</a>
           <h4 className="gallery-item-author">Paulina Blue</h4>
-          <p className="gallery-item-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit, consequatur dicta. Sequi delectus vitae molestias sit odit ullam, iusto commodi minus velit perferendis, beatae quia sed, inventore aperiam error. Perferendis?</p>
+          <p className="gallery-item-blurb">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit, consequatur dicta. Sequi delectus vitae molestias sit odit ullam, iusto commodi minus velit perferendis, beatae quia sed, inventore aperiam error. Perferendis?</p>
         </div>
       </div>
       <div className="gallery-item">
-        <div className="thumb-1"></div>
+        <div className="gallery-item-thumbnail"></div>
         <div className="gallery-item-text">
           <a className="gallery-item-title"></a>
           <h4 className="gallery-item-author"></h4>
-          <p className="gallery-item-text"></p>
+          <p className="gallery-item-blurb"></p>
         </div>
       </div>
       <div className="gallery-item">
-        <div className="thumb-1"></div>
+        <div className="gallery-item-thumbnail"></div>
         <div className="gallery-item-text">
           <a className="gallery-item-title"></a>
           <h4 className="gallery-item-author"></h4>
-          <p className="gallery-item-text"></p>
+          <p className="gallery-item-blurb"></p>
         </div>
       </div>
 

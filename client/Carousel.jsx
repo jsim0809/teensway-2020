145370ff2c765056557fdc37c006aa41d4a5e960
Jeremy Sim <jsim0809@gmail.com>
2020-06-06 22:18:59 -0700

    Implement carousel placeholder

--- client/Carousel.jsx
+++ client/Carousel.jsx
@@ -1,23 +1,52 @@
 import React, { useState } from 'react';
 
 function Carousel() {
+  const [image, setImage] = useState(0);
 
-  return (
-    <section>
-      <div className="splash">
-        {/* // All the splash images
-        <img />
-        <div>
-          <a></a>
-          <a></a>
-        </div>
-
-        // Arrows
-        <a></a>
-        <a></a> */}
-      </div>
-    </section>
-  );
+  switch (image) {
+    case 0:
+      return (
+        <section>
+          <div className="splash-0">
+            <div className="img-0"></div>
+            <div className="splash-text">
+              <a className="splash-author">Ezra Li Parker</a>
+              <a className="splash-title">ALL EAT CANDY</a>
+            </div>
+            <a className="splash-left">&#10094;</a>
+            <a className="splash-right">&#10095;</a>
+          </div>
+        </section>
+      );
+    case 1:
+      return (
+        <section>
+          <div className="splash-1">
+            <div className="img-1"></div>
+            <div className="splash-text">
+              <a className="splash-author">Susan F. Banks</a>
+              <a className="splash-title">DOG EAT DOG</a>
+            </div>
+            <a className="splash-left">&#10094;</a>
+            <a className="splash-right">&#10095;</a>
+          </div>
+        </section>
+      );
+    case 2:
+      return (
+        <section>
+          <div className="splash-2">
+            <div className="img-2"></div>
+            <div className="splash-text">
+              <a className="splash-author">Peter Pan</a>
+              <a className="splash-title">GODS EAT GODS</a>
+            </div>
+            <a className="splash-left">&#10094;</a>
+            <a className="splash-right">&#10095;</a>
+          </div>
+        </section>
+      );
+  }
 }
 
 export default Carousel;

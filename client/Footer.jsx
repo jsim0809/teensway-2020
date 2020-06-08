ab36650c73ba05bf46f60f1932207cf03d13a06e
Jeremy Sim <jsim0809@gmail.com>
2020-06-07 17:04:12 -0700

    Complete front page layout

--- client/Footer.jsx
+++ client/Footer.jsx
@@ -3,8 +3,16 @@ import React, { useState } from 'react';
 function Footer() {
 
   return (
-    <div></div>
-  );
+    <footer>
+      <div className="back-to-top">
+        <a className="back-to-top-arrow">&#10094;</a>
+      </div>
+      <div className="footer-text">
+        <p>©2020 WING LUKE MUSEUM OF THE ASIAN PACIFIC AMERICAN EXPERIENCE</p>
+        <p>THE WING IS A 501(C)(3) NONPROFIT REGISTERED IN THE US UNDER EIN: 91-6067431</p>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer;

ded9e74948ae96ea596b61349d8e882d51160781
Jeremy Sim <jsim0809@gmail.com>
2020-06-14 19:37:13 -0700

    Complete About The Class layout

--- client/AboutTheClass.jsx
+++ client/AboutTheClass.jsx
@@ -3,6 +3,13 @@ import React from 'react';
 function AboutTheClass() {
   return (
     <section>
+      <div className="about-the-class">
+        <div className="about-the-class-text">
+          <h2>ABOUT</h2>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse quo eum sunt, amet soluta repellat. Iusto excepturi quasi natus iste numquam tempore rem, cum ullam. Rem quod mollitia deserunt porro?</p>
+        </div>
+        <div className="about-the-class-image"></div>
+      </div>
     </section>
   );
 }

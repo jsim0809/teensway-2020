7ac236e9305f6417d131e4009b13a996a76fee1c
Jeremy Sim <jsim0809@gmail.com>
2020-06-16 16:53:04 -0700

    Complete Contact layout

--- client/Contact.jsx
+++ client/Contact.jsx
@@ -2,7 +2,28 @@ import React from 'react';
 
 function Contact() {
   return (
-    <section>
+    <section className="contact">
+      <div className="contact-text">
+        <h2>CONTACT</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos magni enim optio ullam, nobis officiis eos qui nesciunt nemo ipsa quae, eveniet ab dicta minus illum voluptatibus maiores veniam amet?</p>
+      </div>
+      <form action="https://formspree.io/mnqgzade" method="POST">
+        <label>
+          <div>Your name</div>
+          <input type="text" name="name" />
+        </label>
+        <label>
+          <div>Your email</div>
+          <input type="text" name="_replyto" />
+        </label>
+        <label>
+          <div>Your message</div>
+          <textarea name="message"></textarea>
+        </label>
+        <div>
+          <button type="submit">Send</button>
+        </div>
+      </form>
     </section>
   );
 }

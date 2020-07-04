995ca93067a825900860a227a8adfd28da8b1498
Jeremy Sim <jsim0809@gmail.com>
2020-07-04 14:03:29 -0700

    Remove a word from Jeremys bio

--- docs/database/teachers.js
+++ docs/database/teachers.js
@@ -6,7 +6,7 @@ module.exports = [
     title: 'Teaching Artist',
     name: 'Jeremy Sim',
     avatar: `${AVATARS}/profilepic_Jeremy.PNG`,
-    bio: `Jeremy Sim is a teacher, writer, software engineer, and pluviophile. He is a graduate of Clarion West Writers Workshop, and is the author of numerous short stories and graphic novels. He served as Teensway's Teaching Artist in 2019 and 2020. Like Luke Skywalker, he is known for drinking green milk from a jar.`,
+    bio: `Jeremy Sim is a teacher, writer, software engineer, and pluviophile. He is a graduate of Clarion West Writers Workshop, and the author of numerous short stories and graphic novels. He served as Teensway's Teaching Artist in 2019 and 2020. Like Luke Skywalker, he is known for drinking green milk from a jar.`,
   },
   {
     id: 1,

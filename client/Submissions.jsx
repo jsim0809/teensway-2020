b7cb5b0f3ab2fccec547caf1a6decd7a9104ea83
Jeremy Sim <jsim0809@gmail.com>
2020-07-04 11:09:09 -0700

    Test Hubspot form

--- client/Submissions.jsx
+++ client/Submissions.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import HubspotForm from 'react-hubspot-form';
 
 function Submissions() {
   return (
@@ -7,7 +8,13 @@ function Submissions() {
         <h2>SUBMISSIONS</h2>
         <p>If you attended Teensway 2020, you can add to your personal comic page by sending Jeremy a message here. You can send in a new comic, a new avatar picture, or a new bio. Or just say hi!</p>
       </div>
-      <form action="https://formspree.io/mnqgzade" method="POST" encType="multipart/form-data">
+
+      <HubspotForm
+        portalId="8038008"
+        formId="f976db6b-2c51-4b0e-92ad-05be4e6a53a3"
+      />
+
+      {/* <form action="https://formspree.io/mnqgzade" method="POST" encType="multipart/form-data">
         <label>
           <div>Your name</div>
           <input type="text" name="name" />
@@ -18,7 +25,7 @@ function Submissions() {
         </label>
         <label>
           <div>Your submission (.png or .jpg format)</div>
-          <input type="file" name="attachment" accept="image/png, image/jpeg"></input>
+          <input type="file" name="upload" accept="image/png, image/jpeg"></input>
         </label>
         <label>
           <div>Your message</div>
@@ -27,7 +34,7 @@ function Submissions() {
         <div>
           <button type="submit">Send</button>
         </div>
-      </form>
+      </form> */}
     </section>
   );
 }

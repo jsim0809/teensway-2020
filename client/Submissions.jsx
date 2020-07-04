7202662422c877988c19bfdd311f2c30e3742ee5
Jeremy Sim <jsim0809@gmail.com>
2020-07-04 11:21:33 -0700

    Style Hubspot form

--- client/Submissions.jsx
+++ client/Submissions.jsx
@@ -9,10 +9,12 @@ function Submissions() {
         <p>If you attended Teensway 2020, you can add to your personal comic page by sending Jeremy a message here. You can send in a new comic, a new avatar picture, or a new bio. Or just say hi!</p>
       </div>
 
-      <HubspotForm
-        portalId="8038008"
-        formId="f976db6b-2c51-4b0e-92ad-05be4e6a53a3"
-      />
+      <div className="contact-form">
+        <HubspotForm
+          portalId="8038008"
+          formId="f976db6b-2c51-4b0e-92ad-05be4e6a53a3"
+        />
+      </div>
 
       {/* <form action="https://formspree.io/mnqgzade" method="POST" encType="multipart/form-data">
         <label>

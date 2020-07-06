d14df8ca32855998763cd4d4fadf1c653ad03354
Jeremy Sim <jsim0809@gmail.com>
2020-07-06 11:46:06 -0700

    Fixed class to className

--- client/Footer.jsx
+++ client/Footer.jsx
@@ -10,7 +10,7 @@ function Footer() {
       <div className="footer-text">
         <p>©2020 WING LUKE MUSEUM OF THE ASIAN PACIFIC AMERICAN EXPERIENCE</p>
         <p className="right-align">THE WING IS A 501(C)(3) NONPROFIT REGISTERED IN THE US UNDER EIN: 91-6067431</p>
-        <a onClick={handleReturnTopClick} className="back-to-top-arrow"><i class="fas fa-chevron-up"></i></a>
+        <a onClick={handleReturnTopClick} className="back-to-top-arrow"><i className="fas fa-chevron-up"></i></a>
       </div>
     </footer>
   );
